0d6249b78e2d7ec036c6874237268b4c3232c8fc
Luana Gonçalves <144810083+luanasgoncalves@users.noreply.github.com>
2025-06-22 00:11:27 -0300

    Update urls.py

--- arcondapp/apps/tecnico/urls.py
+++ arcondapp/apps/tecnico/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 
-app_name = 'cliente'
+app_name = 'tecnico'
 
 router = routers.DefaultRouter()
-router.register('', views.ClienteViewSet, basename='cliente')
+router.register('', views.TecnicoViewSet, basename='tecnico')
 
 urlpatterns = [
     path('', include(router.urls) )
