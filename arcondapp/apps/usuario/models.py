4751339fb583161df67227f3b18e066abe405b54
Luana Gonçalves <144810083+luanasgoncalves@users.noreply.github.com>
2025-06-21 21:08:49 -0300

    feat: cria model da classe Usuário

---
+++ arcondapp/apps/usuario/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Usuario(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    email = models.EmailField('E-mail', unique=True)
+    telefone = models.CharField('Telefone', max_length=20)
+
+    class Meta:
+        verbose_name = 'Usuário'
+        verbose_name_plural = 'Usuários'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.nome
