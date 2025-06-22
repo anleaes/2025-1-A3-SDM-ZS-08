95b029dac8557c90b7c424226432c40e07d4007f
Luana Gonçalves <144810083+luanasgoncalves@users.noreply.github.com>
2025-06-22 01:34:55 -0300

    Update apps.py

--- arcondapp/apps/notificacao/apps.py
+++ arcondapp/apps/notificacao/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class NotificacaoConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "notificacao"
+    verbose_name = 'Notificação'
