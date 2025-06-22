0763949ab25902be98367c4aa2e7c74c0fc696bd
Luana Gonçalves <144810083+luanasgoncalves@users.noreply.github.com>
2025-06-22 00:46:47 -0300

    Update views.py

--- arcondapp/apps/endereco/views.py
+++ arcondapp/apps/endereco/views.py
@@ -4,3 +4,6 @@ from rest_framework import viewsets
 from .serializer import EnderecoSerializer
 
 # Create your views here.
+class EnderecoViewSet(viewsets.ModelViewSet):
+    queryset = Endereco.objects.all()
+    serializer_class = EnderecoSerializer  
