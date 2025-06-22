67829387ff38330d4313ff49bc2e43e63596803d
Luana Gonçalves <144810083+luanasgoncalves@users.noreply.github.com>
2025-06-22 01:40:41 -0300

    Update models.py

--- arcondapp/apps/tecnicoEspecialidade/models.py
+++ arcondapp/apps/tecnicoEspecialidade/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class TecnicoEspecialidade(models.Model):
+    tecnico = models.ForeignKey(Tecnico, on_delete=models.CASCADE, related_name='especialidades')
+    especialidade = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = 'Especialidade de Técnico'
+        verbose_name_plural = 'Especialidades de Técnicos'
