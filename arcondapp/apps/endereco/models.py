ab1ed8d805d18617048ead1ad3ef7dd77db693a6
Luana Gonçalves <144810083+luanasgoncalves@users.noreply.github.com>
2025-06-22 00:43:41 -0300

    Update models.py

--- arcondapp/apps/endereco/models.py
+++ arcondapp/apps/endereco/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from usuario.models import Usuario, Cliente
+from django.utils import timezone
 
 # Create your models here.
 class Endereco(models.Model):
@@ -9,3 +11,10 @@ class Endereco(models.Model):
     cidade = models.CharField(max_length=50)
     estado = models.CharField(max_length=2)
     cep = models.CharField(max_length=10)
+
+class Meta:
+    verbose_name = 'Endereço'
+    verbose_name_plural = 'Endereços'
+
+def __str__(self):
+    return f'{self.rua}, {self.numero} - {self.cidade}/{self.estado}'
