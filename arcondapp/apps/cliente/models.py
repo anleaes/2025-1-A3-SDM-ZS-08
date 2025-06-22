cb9e23318488ee26646e82255c48a5f9fb219288
Luana Gonçalves <144810083+luanasgoncalves@users.noreply.github.com>
2025-06-21 23:12:24 -0300

    Update models.py

--- arcondapp/apps/cliente/models.py
+++ arcondapp/apps/cliente/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from usuario.models import Usuario
 
 # Create your models here.
+
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+    email = models.EmailField('Email', unique=True)
+    telefone = models.CharField('Telefone', max_length=20)
+    endereco = models.CharField('Endereço', max_length=100)
+
+    class Meta:
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
+        ordering = ['id']
