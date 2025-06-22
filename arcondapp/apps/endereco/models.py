3a555196aebf1bd42c2176c0dd9ae2ad34fb1633
Luana Gonçalves <144810083+luanasgoncalves@users.noreply.github.com>
2025-06-22 00:52:33 -0300

    Update models.py

--- arcondapp/apps/endereco/models.py
+++ arcondapp/apps/endereco/models.py
@@ -1,20 +1,15 @@
 from django.db import models
-from usuario.models import Usuario, Cliente
-from django.utils import timezone
+from usuario.models import Usuario  
 
-# Create your models here.
 class Endereco(models.Model):
-    rua = models.CharField(max_length=100)
+    # Definição do seu modelo Endereco
+    logradouro = models.CharField(max_length=200)
     numero = models.CharField(max_length=10)
     complemento = models.CharField(max_length=50, blank=True, null=True)
-    bairro = models.CharField(max_length=50)
-    cidade = models.CharField(max_length=50)
+    bairro = models.CharField(max_length=100)
+    cidade = models.CharField(max_length=100)
     estado = models.CharField(max_length=2)
-    cep = models.CharField(max_length=10)
+    cep = models.CharField(max_length=9)
 
-class Meta:
-    verbose_name = 'Endereço'
-    verbose_name_plural = 'Endereços'
-
-def __str__(self):
-    return f'{self.rua}, {self.numero} - {self.cidade}/{self.estado}'
+    def __str__(self):
+        return f"{self.logradouro}, {self.numero} - {self.bairro}, {self.cidade}/{self.estado}"
