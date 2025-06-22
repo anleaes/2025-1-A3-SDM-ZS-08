cd3a950cdd2ded0c2e783ac5a80214083cdbc761
Luana Gonçalves <144810083+luanasgoncalves@users.noreply.github.com>
2025-06-22 01:35:33 -0300

    Update models.py

--- arcondapp/apps/avaliacao/models.py
+++ arcondapp/apps/avaliacao/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Avaliacao(models.Model):
+    nota = models.PositiveSmallIntegerField()
+    comentario = models.TextField(blank=True)
+    data = models.DateTimeField(default=timezone.now)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    tecnico = models.ForeignKey(Tecnico, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Avaliação'
+        verbose_name_plural = 'Avaliações'
