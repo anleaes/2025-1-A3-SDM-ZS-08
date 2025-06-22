75258fa39153c4908cdf38713c7667f3dbfb76c3
Luana Gonçalves <144810083+luanasgoncalves@users.noreply.github.com>
2025-06-22 01:27:20 -0300

    Update models.py

--- arcondapp/apps/notificacao/models.py
+++ arcondapp/apps/notificacao/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Notificacao(models.Model):
+    DESTINATARIO_CHOICES = [
+        ('Cliente', 'Cliente'),
+        ('Tecnico', 'Técnico'),
+    ]
+
+    destinatario_tipo = models.CharField(max_length=10, choices=DESTINATARIO_CHOICES)
+    destinatario_id = models.PositiveIntegerField()
+    mensagem = models.TextField()
+    data_hora = models.DateTimeField(default=timezone.now)
+
+    def enviar(self):
+        print(f'Notificação enviada para {self.destinatario_tipo} #{self.destinatario_id}: {self.mensagem}')
