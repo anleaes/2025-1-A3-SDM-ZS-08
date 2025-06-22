4dc674a4eea8e7d9856f4572ba910f5cc2438f41
Luana Gonçalves <144810083+luanasgoncalves@users.noreply.github.com>
2025-06-22 00:48:21 -0300

    Update urls.py

--- arcondapp/apps/endereco/urls.py
+++ arcondapp/apps/endereco/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
+
+app_name = 'endereco'
+
+router = routers.DefaultRouter()
+router.register('', views.EnderecoViewSet, basename='endereco')
+
+urlpatterns = [
+    path('', include(router.urls) )
+]
