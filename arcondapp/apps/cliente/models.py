4c0cce586ee0c1a73ca03a3057ef1a7300dc2a20
Luana Gonçalves <144810083+luanasgoncalves@users.noreply.github.com>
2025-06-22 00:36:03 -0300

    Mudando tecnico models e clientes models

--- arcondapp/apps/cliente/models.py
+++ arcondapp/apps/cliente/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-from usuario.models import Usuario, Tecnico
+from usuario.models import Usuario
+# Importe Tecnico do seu próprio aplicativo 'tecnico' se for necessário
+# Se a classe Cliente usar Tecnico, descomente a linha abaixo:
+# from tecnico.models import Tecnico
 
 class Cliente(Usuario):
     endereco = models.CharField('Endereço', max_length=100)
@@ -21,4 +24,5 @@ class Cliente(Usuario):
         print(f'{self.nome} solicitou o serviço: {servico}')
 
     def avaliar_tecnico(self, tecnico, nota, comentario=''):
+        # Se Tecnico for importado acima, este método funcionará
         print(f'{self.nome} avaliou o técnico {tecnico} com nota {nota}. Comentário: {comentario}')
