ea07c86aeff3bdac975666c4d1b98e29023e2d32
Luana Gonçalves <144810083+luanasgoncalves@users.noreply.github.com>
2025-06-23 23:18:25 -0300

    Update serializer.py

--- manutencaoapp/apps/tecnicoEspecialidade/serializer.py
+++ manutencaoapp/apps/tecnicoEspecialidade/serializer.py
@@ -1,12 +1,6 @@
 from .models import TecnicoEspecialidade
-from .models import Especialidade
 
 from rest_framework import serializers
-
-class EspecialidadeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = "Especialidade"
-        fields = '__all__'
 
 class TecnicoEspecialidadeSerializer(serializers.ModelSerializer):
     class Meta:
