838c4f213830bd0d78a3f8e783b4519237891119
Luana Gonçalves <144810083+luanasgoncalves@users.noreply.github.com>
2025-06-23 21:56:47 -0300

    Update views.py

--- manutencaoapp/apps/usuario/views.py
+++ manutencaoapp/apps/usuario/views.py
@@ -1,3 +1,10 @@
+from .models import Usuario
+from rest_framework import viewsets
+from .serializer import UsuarioSerializer
 from django.shortcuts import render
 
 # Create your views here.
+
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer  
