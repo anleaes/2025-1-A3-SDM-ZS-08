eb21eafa2412e9764625eb335ec6e523e435802c
Luana Gonçalves <144810083+luanasgoncalves@users.noreply.github.com>
2025-06-23 23:14:48 -0300

    Update models.py

--- manutencaoapp/apps/tecnicoEspecialidade/models.py
+++ manutencaoapp/apps/tecnicoEspecialidade/models.py
@@ -2,14 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Especialidade(models.Model):
-    nome = models.CharField(max_length=100, unique=True)
-    descricao = models.TextField(blank=True)
-
-    def __str__(self):
-        return self.nome
-
-
 class TecnicoEspecialidade(models.Model):
     tecnico = models.ForeignKey('tecnico.Tecnico', on_delete=models.CASCADE, related_name='especialidades')
     especialidade = models.ForeignKey(Especialidade, on_delete=models.CASCADE, related_name='tecnicos')
