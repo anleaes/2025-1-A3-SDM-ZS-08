8c84e9cb35152dc30fdb8fd3e4c3b833c30f83ca
Luana Gonçalves <144810083+luanasgoncalves@users.noreply.github.com>
2025-06-23 22:41:07 -0300

    Update models.py

--- manutencaoapp/apps/avaliacao/models.py
+++ manutencaoapp/apps/avaliacao/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Avaliacao(models.Model):
+    AVALIADOR_CHOICES = [
+        ('cliente', 'Cliente'),
+        ('tecnico', 'Técnico'),
+    ]
+
+    avaliador_tipo = models.CharField(max_length=10, choices=AVALIADOR_CHOICES)
+    cliente = models.ForeignKey('cliente.Cliente', on_delete=models.CASCADE, related_name='avaliacoes_recebidas')
+    tecnico = models.ForeignKey('tecnico.Tecnico', on_delete=models.CASCADE, related_name='avaliacoes_recebidas')
+    
+    nota = models.PositiveSmallIntegerField()  # Ex: 1 a 5 estrelas
+    comentario = models.TextField(blank=True)
+    data_avaliacao = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.avaliador_tipo.title()} avaliou com {self.nota} estrela(s)"
