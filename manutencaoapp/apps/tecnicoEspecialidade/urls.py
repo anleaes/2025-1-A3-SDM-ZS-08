50eb6e941cbbb1b308bf2f1c909ec7f3d3bb8016
Luana Gonçalves <144810083+luanasgoncalves@users.noreply.github.com>
2025-06-23 23:03:19 -0300

    Update urls.py

---
+++ manutencaoapp/apps/tecnicoEspecialidade/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include
+from . import views
+from rest_framework import routers
+
+app_name = 'tecnicoEspecialidade'
+
+router = routers.DefaultRouter()
+router.register('', views.TecnicoEspecialidadeViewSet, basename='tecnicoEspecialidade')
+
+urlpatterns = [
+    path('', include(router.urls) )
+]
+
+app_name = 'Especialidade'
+
+router = routers.DefaultRouter()
+router.register('', views.TecnicoEspecialidadeViewSet, basename='especialidade')
+
+urlpatterns = [
+    path('', include(router.urls) )
+]
