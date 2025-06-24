cd157f4a59f9fb72d83d7aef9c5e94caac3ac886
Luana Gonçalves <144810083+luanasgoncalves@users.noreply.github.com>
2025-06-23 22:13:27 -0300

    Update views.py

--- manutencaoapp/apps/tecnico/views.py
+++ manutencaoapp/apps/tecnico/views.py
@@ -1,3 +1,10 @@
+from .models import Tecnico
+from rest_framework import viewsets
+from .serializer import TecnicoSerializer
 from django.shortcuts import render
 
 # Create your views here.
+
+class TecnicoViewSet(viewsets.ModelViewSet):
+    queryset = Tecnico.objects.all()
+    serializer_class = TecnicoSerializer  
