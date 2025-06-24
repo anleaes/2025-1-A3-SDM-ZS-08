9b1336cedc8204e7539a70fe9b61e347be564668
Luana Gonçalves <144810083+luanasgoncalves@users.noreply.github.com>
2025-06-23 22:44:58 -0300

    Update views.py

--- manutencaoapp/apps/avaliacao/views.py
+++ manutencaoapp/apps/avaliacao/views.py
@@ -1,3 +1,10 @@
+from .models import Avaliacao
+from rest_framework import viewsets
+from .serializer import AvaliacaoSerializer
 from django.shortcuts import render
 
 # Create your views here.
+
+class AvaliacaoViewSet(viewsets.ModelViewSet):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer  
