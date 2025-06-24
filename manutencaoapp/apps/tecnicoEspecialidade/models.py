9d68f63fc99c8cba5465507a62c8b1265a51d638
Luana Gonçalves <144810083+luanasgoncalves@users.noreply.github.com>
2025-06-23 23:17:46 -0300

    Update models.py

--- manutencaoapp/apps/tecnicoEspecialidade/models.py
+++ manutencaoapp/apps/tecnicoEspecialidade/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+from django.db import models
+
 class TecnicoEspecialidade(models.Model):
     tecnico = models.ForeignKey('tecnico.Tecnico', on_delete=models.CASCADE, related_name='especialidades')
-    especialidade = models.ForeignKey(Especialidade, on_delete=models.CASCADE, related_name='tecnicos')
+    nome_especialidade = models.CharField(max_length=100)
 
     class Meta:
-        unique_together = ('tecnico', 'especialidade')
+        unique_together = ('tecnico', 'nome_especialidade')
 
     def __str__(self):
-        return f"{self.tecnico} - {self.especialidade}"
+        return f"{self.tecnico} - {self.nome_especialidade}"
