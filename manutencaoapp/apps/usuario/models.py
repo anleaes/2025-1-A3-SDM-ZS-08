720f40a739d9abeefd36e3b5362ee84cbed66a81
Luana Gonçalves <144810083+luanasgoncalves@users.noreply.github.com>
2025-06-23 21:41:31 -0300

    Update models.py

--- manutencaoapp/apps/usuario/models.py
+++ manutencaoapp/apps/usuario/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Usuario(models.Model):
-    nome = models.CharField('nome', max_length=255) 
+    nome = models.CharField('nome', max_length=100) 
     email = models.EmailField('E-mail', unique=True, null=False, blank=False) 
     cpf = models.CharField(max_length=14, unique=True,)  
     telefone = models.CharField('Telefone celular', max_length=20)
