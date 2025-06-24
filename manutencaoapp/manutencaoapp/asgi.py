68164afb3c54af30fee7379cc3fba10cfeaa9f7e
Luana Gonçalves <144810083+luanasgoncalves@users.noreply.github.com>
2025-06-23 21:32:05 -0300

    Ambiente criado

--- manutencaoapp/manutencaoapp/asgi.py
+++ manutencaoapp/manutencaoapp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for manutencaoar project.
+ASGI config for manutencaoapp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "manutencaoar.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "manutencaoapp.settings")
 
 application = get_asgi_application()
