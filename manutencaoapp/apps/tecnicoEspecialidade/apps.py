002bbc6ccff991aee5395d4b1112d0d55afb4ef0
Luana Gonçalves <144810083+luanasgoncalves@users.noreply.github.com>
2025-06-23 23:04:14 -0300

    Update apps.py

--- manutencaoapp/apps/tecnicoEspecialidade/apps.py
+++ manutencaoapp/apps/tecnicoEspecialidade/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class TecnicoespecialidadeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "tecnicoEspecialidade"
+
+class EspecialidadeConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "especialidade"
